6f0f19e1f0c9e3081df4ae588624bb4df3d19265
Pranabanti Karmaakar <aankkhiz@gmail.com>
2023-05-16 17:57:29 +0530

    updating to comments

--- redux/action/index.js
+++ redux/action/index.js
@@ -4,20 +4,44 @@ import { ADDNEW_TODO, GETALL_TODO, TOGGLE_TODO, UPDATE_TODO, DELETE_TODO, TOGGLE
 
 
 
-export const addNewTodo = (data) => async (dispatch) => {
+// export const addNewTodo = (data) => async (dispatch) => {
 
+//     const res = await fetch('/api/test/add', {
+//       method: 'POST',
+//       body: JSON.stringify({ data }),
+//       headers: {
+//         'Content-Type': 'application/json'}
+//     });
+
+//     const data1 = await res.json()
+//     console.log(data1)
+
+//     dispatch({ type: ADDNEW_TODO , payload: data })
+
+ 
+// } 
+
+
+export const addNewTodo = (data) => async (dispatch) => {
+  try {
     const res = await fetch('/api/test/add', {
       method: 'POST',
       body: JSON.stringify({ data }),
       headers: {
-        'Content-Type': 'application/json'}
+        'Content-Type': 'application/json'
+      }
     });
 
-    const data1 = await res.json()
-    console.log(data1)
+    if (!res.ok) {
+      throw new Error('Failed to add new todo');
+    }
 
-    dispatch({ type: ADDNEW_TODO , payload: data })
+    const data1 = await res.json();
+    console.log(data1);
 
- 
-} 
+    dispatch({ type: ADDNEW_TODO, payload: data });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
