0b3f9916405aa44c7989309bef9d7b9a04047e9d
Pranabanti Karmaakar <aankkhiz@gmail.com>
2023-05-16 17:19:08 +0530

    updated to npm run buid

--- src/pages/lib/Todo.js
+++ src/pages/lib/Todo.js
@@ -1,56 +1,50 @@
-// import mongoose from "mongoose";
 
-
-// const TodoSchema = new mongoose.Schema({
-//     data: {
-//         type: String,
-//         required: true
-//     },
-//     done: {
-//         type: Boolean,
-//         default: false
-//     },
-//     createdAt: {
-//         type: Date,
-//         default: Date.now
-//     }
-// })
-
-// const todos = _mongoose.model('todos', TodoSchema);
-
-// export default todos;
-
-// import { Schema, model, models } from 'mongoose';
 import mongoose from 'mongoose';
 
-const Todo1Schema = new mongoose.Schema({
-    data: {
-                type: String,
-                required: true
-            },
-            done: {
-                type: Boolean,
-                default: false
-            },
-            createdAt: {
-                type: Date,
-                default: Date.now
+// const Todo1Schema = new mongoose.Schema({
+//     data: {
+//                 type: String,
+//                 required: true
+//             },
+//             done: {
+//                 type: Boolean,
+//                 default: false
+//             },
+//             createdAt: {
+//                 type: Date,
+//                 default: Date.now
 
-            }
+//             }
 
-//     name: String,
-//     email: {
-//     type: String,
-//     // required: true,
-//     // unique: true,
-//   },
 
-//   name: String,
-//   email: String
+// });
 
-});
 
-// const Todo = models.Todo || model('Todo', TodoSchema);
-module.exports = mongoose.models.Todo1 || mongoose.model('Todo1', Todo1Schema)
+// module.exports = mongoose.models.Todo1 || mongoose.model('Todo1', Todo1Schema);
+// // module.exports = mongoose.models.Todo1 || mongoose.model('Todo1', Todo1Schema);
 
-// export default Todo;
+
+
+module.exports = () => {
+  
+
+    const Todo1Schema = new mongoose.Schema({
+        data: {
+                    type: String,
+                    required: true
+                },
+                done: {
+                    type: Boolean,
+                    default: false
+                },
+                createdAt: {
+                    type: Date,
+                    default: Date.now
+    
+                }
+    
+    
+    });
+   
+    return mongoose.model('Todo1', Todo1Schema);
+   }
